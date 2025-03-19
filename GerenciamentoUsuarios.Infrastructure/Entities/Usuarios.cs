4e1e79dcf851ec1087ff44066539542ac73ec596
JanseyScofield <janscofield10@gmail.com>
2025-03-18 23:51:07 -0300

    Configurando o banco de dados adicionando a tabela de Usuarios

---
+++ GerenciamentoUsuarios.Infrastructure/Entities/Usuarios.cs
@@ -0,0 +1,10 @@
+﻿namespace GerenciamentoUsuarios.Infrastructure.Entities
+{
+    public class Usuarios
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Idade { get; set; }
+    }
+}
---
+++ GerenciamentoUsuarios.Infrastructure/UsuariosDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GerenciamentoUsuarios.Infrastructure.Entities;
+
+namespace GerenciamentoUsuarios.Infrastructure
+{
+    public class UsuariosDbContext : DbContext
+    {
+        public DbSet<Usuarios> Usuario { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "gerenciamento-usuarios", "users.db")}");
+        }
+    }
+}
