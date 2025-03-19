f5de47bcef8f97fb5775c26fc8dddfff8dbfe015
JanseyScofield <janscofield10@gmail.com>
2025-03-19 12:04:06 -0300

    Adicionando classes para representar requests e responses na aplicação de communication

---
+++ GerenciamentoUsuarios.Communication/Requests/RequestRegisterUsuariosJson.cs
@@ -0,0 +1,9 @@
+﻿namespace GerenciamentoUsuarios.Communication.Requests
+{
+    public class RequestRegisterUsuariosJson
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Idade { get; set; }
+    }
+}
---
+++ GerenciamentoUsuarios.Communication/Responses/ResponseUsuarioDetailsJson.cs
@@ -0,0 +1,10 @@
+﻿namespace GerenciamentoUsuarios.Communication.Responses
+{
+    public class ResponseUsuarioDetailsJson
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Idade { get; set; }
+    }
+}
